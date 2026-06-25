afb12e0259f3df6eb30db6d5120f77910fc89dec
b1432918 <91201626+b1432918@users.noreply.github.com>
2026-06-25 12:35:04 -0400

    ADD TRAILS

--- src/components/useTrails.js
+++ src/components/useTrails.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../supabase";
 
 export default function useTrails() {
   const [trails, setTrails] = useState([]);
